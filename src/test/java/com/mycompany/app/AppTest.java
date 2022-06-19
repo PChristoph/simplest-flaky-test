3859a8b16bf6ce2156140a027c03da4efc29d612
SEKAS\poeppelbaum <poeppelbaum@sekas.de>
2022-06-19 18:31:43 +0200

    Changed to Juni4 notation and added surefire plugin.

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,45 +1,27 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.Random;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
+    @Test
     public void testAlwaysValid()
     {
         assertTrue( true );
     }
 
+    @Test
     public void testAlwaysInvalid()
     {
         assertTrue( false );
     }
 
+    @Test
     public void testFlaky()
     {
         int max = 4;
